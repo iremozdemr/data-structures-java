0cd0787bc9aa3df877f3f1c8fdd2106f20d699be
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-06-19 17:50:10 +0300

    .class files

--- queue/josephus/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
- CircularQueue<String> queue2 = new CircularQueue<>();
-        queue2.enqueue("a");
-        queue2.enqueue("b");
-
-        String winner2 = josephus(queue1,7);
-        System.out.println("the winner is " + winner2);
